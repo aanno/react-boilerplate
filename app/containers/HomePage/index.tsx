4dc090cd99666188555637660811958b224c0231
Thomas Pasch <thomas.pasch@siteos.de>
2017-03-10 09:20:38 +0100

    fixes

--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -10,9 +10,9 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
+import { makeSelectRepos, makeSelectLoading, makeSelectError } from '../../containers/App/selectors';
+import H2 from '../../components/H2';
+import ReposList from '../../components/ReposList';
 import AtPrefix from './AtPrefix';
 import CenteredSection from './CenteredSection';
 import Form from './Form';
